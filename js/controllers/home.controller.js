b36d78d7dc65fd0f9661918236e39f593f4ee9c3
Yunior González <dev@getinko.com>
2017-05-21 19:35:31 +0100

    Init angular - First controller and module

---
+++ js/controllers/home.controller.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+    angular
+        .module('AngularJS')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['$scope'];
+    function HomeController($scope) {
+        $scope.title = 'Hola mundo';
+
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
