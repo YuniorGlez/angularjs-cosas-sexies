bda72d656cfa37335190b452be710335e09c0ab0
Yunior González <dev@getinko.com>
2017-05-21 19:41:04 +0100

    Init Angular-route, first route to home

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 (function() {
     'use strict';
 
-    angular.module('AngularJS', []);
+    angular.module('AngularJS', ['ngRoute'])
+        .config(routes);
+
+
+    routes.$inject = ['$routeProvider', '$locationProvider'];
+
+    function routes($routeProvider) {
+        $routeProvider.
+            when('/', {
+                controller: 'HomeController',
+                template : '<h1> {{title}} {{2020 - 20}} </h1>'
+            })
+    }
 })();
